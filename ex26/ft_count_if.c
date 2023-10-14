ddb95e02be4991df79dac55d6786c82ad8913271
João V. Mendes <mendes.silva.jv@gmail.com>
2023-10-14 01:50:41 -0300

    Update ft_count_if.c

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -12,13 +12,11 @@
 
 int	ft_count_if(char **tab, int (*f)(char*))
 {
-	int	i;
 	int	count;
 
-	i = -1;
 	count = 0;
-	while (tab[++i] != 0)
-		if (f(tab[i]) == 1)
+	while (*array)
+		if (f(*(array++)))
 			count++;
 	return (count);
 }
